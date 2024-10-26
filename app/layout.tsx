9e2f84e1212fd345c903894a6701653dda329460
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-10-26 14:53:00 +0100

    feat/ add inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,10 @@ import './globals.css';
 import { Providers } from '@/providers/ThemeProvider';
 import  ReduxProvider  from '@/providers/redux-provider';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+import { Inter, Instrument_Sans } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+const instrument = Instrument_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -28,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <Providers>
           <ReduxProvider>
